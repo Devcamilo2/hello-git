7291e401bf8e2063b5139ad1f995da878ccf4b8a
CamiloDev <cardonacamilo624@gmail.com>
2026-02-16 14:57:35 -0500

    Hello Github

--- hellogit.py
+++ hellogit.py
@@ -1,3 +1,4 @@
 # type nul > hellogit.py para crear un archivo vacío en Windows 
+#mkdir git-hello para crear un nuevo directorio llamado git-hello (solo funciona en linux)
 
-print("New Hello Git!")
+print("New Hello GitHub!")
